bd7e933973e02c758d3ccb88011afd4e005b205d
Jeroen Cappelle <jeroencappelle@outlook.com>
2018-02-11 11:22:42 +0100

    now staged

--- chef/static/scripts.js
+++ chef/static/scripts.js
@@ -27,13 +27,14 @@ $(function () {
       var geojson = L.geoJson(data, {style: myStyle}).addTo(mymap);
     
         geojson.eachLayer(function (layer) {
-            layer.bindPopup((country = layer.feature.properties.name) + '<div class="row"><button type="button" id="recipes-button" class="btn btn-default" data-toggle="modal" data-target="#myModal" onclick="showRecipes(' + "'" + country.toLowerCase() + "'" + ')">Show recipes</button>');
+            layer.bindPopup((this.country = layer.feature.properties.name) + '<div class="row"><button type="button" id="recipes-button" class="btn btn-default" data-toggle="modal" data-target="#myModal" onclick="showRecipes(' + "'" + country.toLowerCase() + "'" + ')">Show recipes</button>');
         });
     });
 
 });
 
 function showRecipes(country) {
+	this.country = country;
 	document.getElementById("recipe-list").innerHTML = '<img class="center-block" id="loader" alt="loading" src="/static/ajax-loader.gif"/>';
 	var content = '';
 	
@@ -46,6 +47,10 @@ function showRecipes(country) {
 			content += ('<div class="media"><div class="media-left"><a href="' + item.recipe.url  + '" target="_blank">' + '<img class="media-object" src="' + item.recipe.image + '"alt="recipe image"></a></div>' + '<div class="media-body"><h4 class="media-heading">' + item.recipe.label + '</h4><b>Ingredients: </b>' + item.recipe.ingredientLines + '<p><i>Tags: ' + item.recipe.healthLabels + '</i></p>' + '</div></div>');
 		})
 		
+		if (data.hits.length < 1) {
+			updatePagination(-1);	
+		}
+		
 		// paste contenstring in modal
 		document.getElementById("recipe-list").innerHTML = content;
 	})
@@ -55,4 +60,14 @@ function showRecipes(country) {
 		console.log(errorThrown.toString());
 		}
 	);
+}
+
+function updatePagination(change) {
+	pageIndex = pageIndex + change;
+	if (pageIndex < 1) {
+		document.getElementById("previous").className = 'disabled';
+	} else {
+		document.getElementById("previous").className = '';
+	}
+	showRecipes(country);
 }
