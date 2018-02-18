e6aa72a23df03671c9d39ed1686c9e1fc79439c3
Jeroen Cappelle <jeroencappelle@outlook.com>
2018-02-18 21:47:03 +0100

    add media queries

--- chef/static/scripts.js
+++ chef/static/scripts.js
@@ -9,13 +9,21 @@ var pageIndex = 0;
 
 // execute when the DOM is fully loaded
 $(function () {
+	
+	var corner1 = L.latLng(85, -180),
+	corner2 = L.latLng(-85, 180),
+	bounds = L.latLngBounds(corner1, corner2);
 
-    mymap = L.map('mapid').setView([51.505, -0.09], 2);
+    mymap = L.map('mapid', {
+		maxBounds: bounds
+	}).setView([51.505, -0.09], 2);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/capp88/cjb42s7nm0bdd2rmsgotqsc15/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2FwcDg4IiwiYSI6ImNqYjE2NDV0ejgxYW0yd3A4eTRvZGN5aDAifQ.dALOOghO3sHg8Wa1gnbR3Q', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
-        zoom: 2
+		minZoom: 1,
+        zoom: 2,
+		maxBounds: bounds
     }).addTo(mymap);
     
     var myStyle = {
@@ -51,7 +59,7 @@ function showRecipes(country) {
 
 		// iterate over array of objects and create HTML template
 		$.each(data.hits, function(i, item) {
-			content += ('<div class="media"><div class="media-left"><a href="' + item.recipe.url  + '" target="_blank">' + '<img class="media-object" src="' + item.recipe.image + '"alt="recipe image"></a></div>' + '<div class="media-body"><h4 class="media-heading">' + item.recipe.label + '</h4><b>Ingredients: </b>' + item.recipe.ingredientLines + '<p><i>Tags: ' + item.recipe.healthLabels + '</i></p>' + '</div></div>');
+			content += ('<div class="media"><div class="media-left"><a href="' + item.recipe.url  + '" target="_blank">' + '<img class="media-object" src="' + item.recipe.image + '"alt="recipe image"></a></div>' + '<div class="media-body"><h4 class="media-heading">' + item.recipe.label + '</h4><p><b>Ingredients: </b>' + item.recipe.ingredientLines + '</p><p><i>Tags: ' + item.recipe.healthLabels + '</i></p>' + '</div></div>');
 		})
 		
 		if (data.hits.length < 1) {
